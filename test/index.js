00061b496ca264390b3fa8087162b5f0008155c2
Kevin Simper <kevin.simper@gmail.com>
2016-09-07 14:03:06 +0200

    Update to support post requests also

--- test/index.js
+++ test/index.js
@@ -2,3 +2,9 @@ var Hyper = require('../dist')
 
 let api = new Hyper()
 api.get('/containers/json').then(c => console.log(c))
+
+let createOptions = {
+  Image: 'ubuntu',
+  Cmd: 'date'
+}
+api.post('/containers/create', createOptions).then(c => console.log(c)).catch((e) => console.log(e))
